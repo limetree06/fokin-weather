3929ad8e7e3a1b4654659841bc5e4d08bb607726
limetree06 <limetree06@unist.ac.kr>
2021-10-04 00:45:38 +0900

    style

--- study/App.js
+++ study/App.js
@@ -47,20 +47,54 @@ class App extends Component {
 
   render() {
     return (
-      <View style={styles.background}>
-        <PropsChild
-          sampleText={this.state.sampleText}
-          changeState={this.changeState}
-        />
+      <View style={styles.mainView}>
+        <View style={styles.subView}>
+          <Text style={styles.mainText}>Hello World</Text>
+        </View>
+        <View style={styles.subView}>
+          <PropsChild
+            sampleText={this.state.sampleText}
+            changeState={this.changeState}
+          />
+        </View>
+        <View style={styles.anothersubView}>
+          <PropsChild
+            sampleText={this.state.sampleText}
+            changeState={this.changeState}
+          />
+        </View>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
-  background: {
+  mainView: {
     flex: 1,
+    backgroundColor: 'green',
+    paddingTop: 50,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  subView: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '50%',
+  },
+  anothersubView: {
+    flex: 2,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainText: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: 'red',
   },
 });
 
