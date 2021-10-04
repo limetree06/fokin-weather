869e2663fdf1189be71f9a9c4a694fa2beac2e03
limetree06 <limetree06@unist.ac.kr>
2021-10-05 00:11:45 +0900

    textinput

--- weather/navigation/classScreen.js
+++ weather/navigation/classScreen.js
@@ -1,14 +1,62 @@
-import React, {Component} from 'react';
-import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
+import React, {useState} from 'react';
+import {theme} from './color';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  TouchableHighlight,
+  pressable,
+} from 'react-native';
 
-class ClassScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text>ClassScreen</Text>
+function ClassScreen() {
+  const [working, setWorking] = useState(true);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{
+              ...styles.buttonText,
+              color: working ? 'white' : theme.grey,
+            }}>
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.buttonText,
+              color: !working ? 'white' : theme.grey,
+            }}>
+            Travel
+          </Text>
+        </TouchableOpacity>
       </View>
-    );
-  }
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    paddingHorizontal: 30,
+  },
+  header: {
+    flexDirection: 'row',
+    marginTop: 50,
+    justifyContent: 'space-between',
+  },
+  buttonText: {
+    fontSize: 44,
+    fontWeight: '600',
+  },
+});
 
 export default ClassScreen;
--- weather/navigation/homeScreen.js
+++ weather/navigation/homeScreen.js
@@ -1,14 +1,92 @@
-import React, {Component} from 'react';
-import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import * as Location from 'expo-location';
+import {Text, View, StyleSheet, ScrollView, Dimensions} from 'react-native';
 
-class HomeScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text>HomeScreen</Text>
-      </View>
+const {width: SCREEN_WIRDTH} = Dimensions.get('window');
+
+function HomeScreen() {
+  const [city, setCity] = useState('Loading...');
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: {latitude, longitude},
+    } = await Location.getCurrentPositionAsync({accuracy: 5});
+    const location = await Location.reverseGeocodeAsync(
+      {latitude, longitude},
+      {useGoogleMaps: false},
     );
-  }
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: 'pink'},
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {fontSize: 48},
+  weather: {},
+
+  day: {
+    width: SCREEN_WIRDTH,
+    alignItems: 'center',
+  },
+  temp: {
+    fontSize: 180,
+    fontWeight: '600',
+    marginTop: 50,
+  },
+
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+    fontWeight: '600',
+  },
+});
 
 export default HomeScreen;
